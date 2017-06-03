d8ebb83f06630cec5e75bebd652c87e65a23ff79
KidMaxwell <paul.reck@outlook.com>
2017-06-03 16:02:31 +0200

    added column select

--- src/FlipDot.cpp
+++ src/FlipDot.cpp
@@ -14,6 +14,11 @@ using namespace std;
 
 #define enable 0
 #define data 1
+#define a0 2
+#define a1 3
+#define a2 4
+#define b1 5
+#define b2 6
 
 int main() {
 	cout << "Programm wird gestartet" << endl;
@@ -21,8 +26,11 @@ int main() {
 	int in = 0;
 
 	wiringPiSetup();
-	pinMode(enable, OUTPUT);
-	pinMode(data, OUTPUT);
+
+	for (int i=0; i<7; i++) {
+		pinMode(i, OUTPUT);
+		digitalWrite(i, 0);
+	}
 
 	for (;;) {
 		cout << "Data:" << endl;
@@ -54,4 +62,150 @@ int main() {
 
 	}
 	return 0;
+}
+
+void selectColumn(string column) {
+
+	for (int i=0; i<7; i++) {
+			digitalWrite(i, 0);
+		}
+
+	switch(column) {
+	case 'aa':
+		digitalWrite(a0, 1);
+		break;
+	case 'ab':
+		digitalWrite(a1, 1);
+		break;
+	case 'ac':
+		digitalWrite(a0, 1);
+		digitalWrite(a1, 1);
+		break;
+	case 'ad':
+		digitalWrite(a2, 1);
+		break;
+	case 'ae':
+		digitalWrite(a0, 1);
+		digitalWrite(a2, 1);
+		break;
+	case 'af':
+		digitalWrite(a1, 1);
+		digitalWrite(a2, 1);
+		break;
+	case 'ag':
+		digitalWrite(a0, 1);
+		digitalWrite(a1, 1);
+		digitalWrite(a2, 1);
+		break;
+	case 'ah':
+		digitalWrite(a0, 1);
+		digitalWrite(b1, 1);
+		break;
+	case 'ai':
+		digitalWrite(a1, 1);
+		digitalWrite(b1, 1);
+		break;
+	case 'aj':
+		digitalWrite(a0, 1);
+		digitalWrite(a1, 1);
+		digitalWrite(b1, 1);
+		break;
+	case 'ak':
+		digitalWrite(a2, 1);
+		digitalWrite(b1, 1);
+		break;
+	case 'al':
+		digitalWrite(a0, 1);
+		digitalWrite(a2, 1);
+		digitalWrite(b1, 1);
+		break;
+	case 'am':
+		digitalWrite(a1, 1);
+		digitalWrite(a2, 1);
+		digitalWrite(b1, 1);
+		break;
+	case 'an':
+		digitalWrite(a0, 1);
+		digitalWrite(a1, 1);
+		digitalWrite(a2, 1);
+		digitalWrite(b1, 1);
+		break;
+	case 'ao':
+		digitalWrite(a0, 1);
+		digitalWrite(b2, 1);
+		break;
+	case 'ap':
+		digitalWrite(a1, 1);
+		digitalWrite(b2, 1);
+		break;
+	case 'aq':
+		digitalWrite(a0, 1);
+		digitalWrite(a1, 1);
+		digitalWrite(b2, 1);
+		break;
+	case 'ar':
+		digitalWrite(a2, 1);
+		digitalWrite(b2, 1);
+		break;
+	case 'as':
+		digitalWrite(a0, 1);
+		digitalWrite(a2, 1);
+		digitalWrite(b2, 1);
+		break;
+	case 'at':
+		digitalWrite(a1, 1);
+		digitalWrite(a2, 1);
+		digitalWrite(b2, 1);
+		break;
+	case 'au':
+		digitalWrite(a0, 1);
+		digitalWrite(a1, 1);
+		digitalWrite(a2, 1);
+		digitalWrite(b2, 1);
+		break;
+	case 'av':
+		digitalWrite(a0, 1);
+		digitalWrite(b1, 1);
+		digitalWrite(b2, 1);
+		break;
+	case 'aw':
+		digitalWrite(a1, 1);
+		digitalWrite(b1, 1);
+		digitalWrite(b2, 1);
+		break;
+	case 'ax':
+		digitalWrite(a0, 1);
+		digitalWrite(a1, 1);
+		digitalWrite(b1, 1);
+		digitalWrite(b2, 1);
+		break;
+	case 'ay':
+		digitalWrite(a2, 1);
+		digitalWrite(b1, 1);
+		digitalWrite(b2, 1);
+		break;
+	case 'az':
+		digitalWrite(a0, 1);
+		digitalWrite(a2, 1);
+		digitalWrite(b1, 1);
+		digitalWrite(b2, 1);
+		break;
+	case 'ba':
+		digitalWrite(a1, 1);
+		digitalWrite(a2, 1);
+		digitalWrite(b1, 1);
+		digitalWrite(b2, 1);
+		break;
+	case 'bb':
+		digitalWrite(a0, 1);
+		digitalWrite(a1, 1);
+		digitalWrite(a2, 1);
+		digitalWrite(b1, 1);
+		digitalWrite(b2, 1);
+		break;
+	default:
+		cout<< 'incorrect column value' << endl;
+	}
+
+
 }
