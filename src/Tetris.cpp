29a31846fbc3ab40bebd1fe9699426ec8bb1f1d6
guenterin <alexander_hefele@t-online.de>
2017-09-09 16:22:05 +0000

    TetrisElements erzeugt

---
+++ src/Tetris.cpp
@@ -0,0 +1,14 @@
+/*
+ * Tetris.cpp
+ *
+ *  Created on: 09.09.2017
+ *      Author: alexander
+ */
+
+#include "Tetris.h"
+
+Tetris::Tetris() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ src/Tetris.h
@@ -0,0 +1,16 @@
+/*
+ * Tetris.h
+ *
+ *  Created on: 09.09.2017
+ *      Author: alexander
+ */
+
+#ifndef TETRIS_H_
+#define TETRIS_H_
+
+class Tetris {
+public:
+	Tetris();
+};
+
+#endif /* TETRIS_H_ */
