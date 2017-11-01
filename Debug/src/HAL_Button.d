432361bebdfc524004312904839bb89e3b12c83c
KidMaxwell <paul.reck@outlook.com>
2017-11-01 23:03:02 +0100

    Lauffähig + altes löschen

---
+++ Debug/src/HAL_Button.d
@@ -0,0 +1,9 @@
+src/HAL_Button.o: ../src/HAL_Button.cpp ../src/HAL_Button.h \
+ /home/paul/eclipse/wiringPi/wiringPi/wiringPi.h \
+ ../src/HAL_HardwareDefines.h
+
+../src/HAL_Button.h:
+
+/home/paul/eclipse/wiringPi/wiringPi/wiringPi.h:
+
+../src/HAL_HardwareDefines.h:
--- src/HAL_Button.cpp
+++ src/HAL_Button.cpp
@@ -29,25 +29,25 @@ HAL_Button::buttonHits_Tetris HAL_Button::readButton(int speed) {
 		input_Array[n] = 0;
 		prev_input_Array[n] = 0;
 	}
-	for (int counter = speed * TETRIS_UPDATE_SPEED; counter == 0; counter--) {
-		// Eingang einlesen, mit vorherigen Abgleichen und evlt. button_hit erhöhen
-		input_Array[0] = digitalRead(B_RIGHT);
-		if (input_Array[0] == 1 && prev_input_Array[0] == 0)
-			button_hits.b_right++;
-		input_Array[1] = digitalRead(B_LEFT);
-		if (input_Array[1] == 1 && prev_input_Array[1] == 0)
-					button_hits.b_left++;
-		input_Array[2] = digitalRead(B_UP);
-		if (input_Array[2] == 1 && prev_input_Array[2] == 0)
-					button_hits.b_rotate++;
-		input_Array[3] = digitalRead(B_DOWN);
-		if (input_Array[3] == 1 && prev_input_Array[3] == 0)
-					button_hits.b_movedown++;
-		// aktuellen Input in prev_Input schreiben
-		for (int n = 0; n < 4; n++) {
-			prev_input_Array[n] = input_Array[n];
-		}
-	}
+//	for (int counter = speed * TETRIS_UPDATE_SPEED; counter == 0; counter--) {
+//		// Eingang einlesen, mit vorherigen Abgleichen und evlt. button_hit erhöhen
+//		input_Array[0] = digitalRead(B_RIGHT);
+//		if (input_Array[0] == 1 && prev_input_Array[0] == 0)
+//			button_hits.b_right++;
+//		input_Array[1] = digitalRead(B_LEFT);
+//		if (input_Array[1] == 1 && prev_input_Array[1] == 0)
+//					button_hits.b_left++;
+//		input_Array[2] = digitalRead(B_UP);
+//		if (input_Array[2] == 1 && prev_input_Array[2] == 0)
+//					button_hits.b_rotate++;
+//		input_Array[3] = digitalRead(B_DOWN);
+//		if (input_Array[3] == 1 && prev_input_Array[3] == 0)
+//					button_hits.b_movedown++;
+//		// aktuellen Input in prev_Input schreiben
+//		for (int n = 0; n < 4; n++) {
+//			prev_input_Array[n] = input_Array[n];
+//		}
+//	}
 }
 
 int HAL_Button::readButtonLast() {
